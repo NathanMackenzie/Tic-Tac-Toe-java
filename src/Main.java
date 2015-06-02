5aedc294419778cca521c198292cd94242cfef72
weemackee <weemackee@gmail.com>
2015-06-02 19:40:20 -0400

    Mouse listener added

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,15 @@ import javax.swing.JFrame;
 
 public class Main extends JFrame{
 	public Main(){
-		Window window = new Window();
 		setSize(500, 500);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
-		add(window);
 		setVisible(true);
+		Window window = new Window();
+		window.setLocation(100, 100);
+		add(window);
+		
+		
 	}
 	
 	public static void main(String [] args){
--- src/Window.java
+++ src/Window.java
@@ -1,10 +1,78 @@
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
 import javax.swing.JPanel;
 
 
-public class Window extends JPanel{
+public class Window extends JPanel implements MouseListener, MouseMotionListener{
+	private static int x;
+	private static int y;
+	
+	public Window(){
+		setSize(235, 235);
+		setBackground(new Color(255, 255, 255));
+		x = 0;
+		y = 0;
+	}
 
 	@Override public void paintComponent(Graphics g){
-		g.drawRect(100, 100, 10, 10);
+		super.paintComponent(g);
+		paintGameBoard(g);
+		paintSelected(g);
+		paintTiles(g);
+	}
+	
+	/**
+	 * Paints game board outline
+	 * 
+	 * @param g
+	 */
+	private void paintGameBoard(Graphics g){
+		g.fillRect(75, 0, 5, 235);
+		g.fillRect(155, 0, 5, 235);
+		g.fillRect(0, 75, 235, 5);
+		g.fillRect(0, 155, 235, 5);	
+	}
+	
+	
+	private void paintSelected(Graphics g){
+		
+	}
+	
+	private void paintTiles(Graphics g){
+		
+	}
+	
+	public void mouseMoved(MouseEvent e) {
+	      x = e.getX();
+	      y = e.getY();
+	      repaint();
+	}
+
+	public void mouseDragged(MouseEvent e) {
+	      
+	}
+	
+	public void mousePressed(MouseEvent e) {
+		
+	}
+
+	public void mouseReleased(MouseEvent e) {
+		
+	}
+
+	public void mouseEntered(MouseEvent e) {
+		
+	}
+
+	public void mouseExited(MouseEvent e) {
+		
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		
 	}
 }
